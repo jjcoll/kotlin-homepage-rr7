deeb3dd9970cfaeb1db19ccbb177a83ca7ab6d13
jjcoll <jjordicoll@gmail.com>
2026-03-08 09:36:43 +0100

    init React Router 7 project

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,11 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+// routes are configured in this file
+// routes are made up of two parts, a url pattern + file path to route module 
+
+export default [
+  // renders into the root.tsx Outlet at /
+  index("routes/home.tsx")
+] satisfies RouteConfig;
+
+
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,23 @@
+// This is a route module
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+// loader: provide data to route components before they are rendered 
+// usually usually called on server when SSR or during build during pre-rendering (SSG)
+export async function loader() {
+  return { message: "Hello World!" }
+}
+
+
+// default export in route module defines the component that will render when the route matches
+export default function Home({ loaderData }: Route.ComponentProps) {
+  return (
+    <p>{loaderData.message}</p>
+  )
 }
